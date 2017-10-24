402a3ca325cfbe2f2433807902b4781b9bd77f0b
Husso1997 <hussain1961@hotmail.com>
2017-10-24 11:31:19 +0200

    fxml
    fxml

--- src/kaffemaskine2/gui/Kaffemaskine2.java
+++ src/kaffemaskine2/gui/Kaffemaskine2.java
@@ -1,11 +1,9 @@
 package kaffemaskine2.gui;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 /**
@@ -16,28 +14,13 @@ public class Kaffemaskine2 extends Application
 {
 
     @Override
-    public void start(Stage primaryStage)
-    {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>()
-        {
-
-            @Override
-            public void handle(ActionEvent event)
-            {
-                System.out.println("Hello World!");
-            }
-        });
-
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-
-        Scene scene = new Scene(root, 300, 250);
-
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("view/KaffeMaskine2.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
     }
 
     /**
