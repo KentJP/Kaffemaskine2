ddbdd1417f71af34b6bfdaac5b3abd63f23207d3
Makosful <32297912+Makosful@users.noreply.github.com>
2017-10-24 12:10:12 +0200

    Added sleep functions for extra realism

--- src/kaffemaskine2/gui/controller/KaffeMaskine2Controller.java
+++ src/kaffemaskine2/gui/controller/KaffeMaskine2Controller.java
@@ -42,29 +42,38 @@ public class KaffeMaskine2Controller implements Initializable
      * @param event Retrieves the button
      */
     @FXML
-    private void makeDrink(ActionEvent event)
+    private void makeDrink(ActionEvent event) throws InterruptedException
     {
         if (event.getSource().equals(btnKaffe))
         {
             System.out.println("Laver kaffe");
+            Thread.sleep(20000);
+            System.out.println("Værsgo");
         }
         else if (event.getSource().equals(btnKakao))
         {
             System.out.println("Laver Kakao");
+            Thread.sleep(20000);
+            System.out.println("Værsgo");
         }
         else if (event.getSource().equals(btnCapucino))
         {
             System.out.println("Laver Capucino");
+            Thread.sleep(20000);
+            System.out.println("Værsgo");
         }
         else if (event.getSource().equals(btnCaffeLatte))
         {
             System.out.println("Laver CaffeLatte");
+            Thread.sleep(20000);
+            System.out.println("Værsgo");
         }
         else
         {
             // If none of the above matches, throw an error
             System.out.println("Error");
         }
+        System.out.println("");
     }
 
 }
