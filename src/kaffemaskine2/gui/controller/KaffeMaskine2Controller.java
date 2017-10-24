4489a21f589963d0cbcc4c0e3c1ad92a45baac07
Makosful <32297912+Makosful@users.noreply.github.com>
2017-10-24 11:49:35 +0200

    Lavet metoden der håndtere hvilken kasse at lave

--- src/kaffemaskine2/gui/controller/KaffeMaskine2Controller.java
+++ src/kaffemaskine2/gui/controller/KaffeMaskine2Controller.java
@@ -1,27 +1,64 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package kaffemaskine2.gui.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 
 /**
  * FXML Controller class
  *
  * @author Hussain
  */
-public class KaffeMaskine2Controller implements Initializable {
+public class KaffeMaskine2Controller implements Initializable
+{
+
+    @FXML
+    private Button btnKaffe;
+    @FXML
+    private Button btnKakao;
+    @FXML
+    private Button btnCapucino;
+    @FXML
+    private Button btnCaffeLatte;
 
     /**
      * Initializes the controller class.
+     *
+     * @param url
+     * @param rb
      */
     @Override
-    public void initialize(URL url, ResourceBundle rb) {
+    public void initialize(URL url, ResourceBundle rb)
+    {
         // TODO
-    }    
-    
+    }
+
+    @FXML
+    private void makeDrink(ActionEvent event)
+    {
+        if (event.getSource().equals(btnKaffe))
+        {
+            System.out.println("Laver kaffe");
+        }
+        else if (event.getSource().equals(btnKakao))
+        {
+            System.out.println("Laver Kakao");
+        }
+        else if (event.getSource().equals(btnCapucino))
+        {
+            System.out.println("Laver Capucino");
+        }
+        else if (event.getSource().equals(btnCaffeLatte))
+        {
+            System.out.println("Laver CaffeLatte");
+        }
+        else
+        {
+            System.out.println("Error");
+        }
+    }
+
 }
